66cd81b47acad3718ece34a110cbd3881f018b1f
Chu Yong <415161584@qq.com>
2021-05-17 11:38:50 +0800

    add

---
+++ com.misa.npm/Runtime/NPM.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Misa
+{
+    public static class NPM 
+    {
+        public const string myblog = "https://blog.csdn.net/mseol";
+    }
+}
